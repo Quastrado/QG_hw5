266dea8d0d96274c4c1053e89a1561ed145c403e
Quastrado <Ataxyswriter@yandex.ru>
2026-04-07 21:36:14 +0300

    testdata for TextBox tests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -5,28 +5,29 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static tests.testdata.TestDataTextBox.*;
 
 public class TextBoxTests extends TestBase {
 
     @Test // Успешное заполнение и отправка формы
     void successfulFormCompletionTest(){
         open("/text-box.html");
-        $("input[id=userName]").setValue("Full Name");
-        $("input[id=userEmail]").setValue("example@mail.com");
-        $("textarea[id=currentAddress]").setValue("Pushkina, Kolotushkina");
-        $("textarea[id=permanentAddress]").setValue("Pushkina, Kolotushkina");
+        $("input[id=userName]").setValue(userName);
+        $("input[id=userEmail]").setValue(userEmail);
+        $("textarea[id=currentAddress]").setValue(address);
+        $("textarea[id=permanentAddress]").setValue(address);
         $("button[id=submit]").click();
 
-        $("p[id=name]").shouldHave(text("Name:Full Name"));
-        $("p[id=email]").shouldHave(text("Email:example@mail.com"));
-        $("p[id=currentAddress]").shouldHave(text("Current Address :Pushkina, Kolotushkina"));
-        $("p[id=permanentAddress]").shouldHave(text("Permananet Address :Pushkina, Kolotushkina"));
+        $("p[id=name]").shouldHave(text(expectedName));
+        $("p[id=email]").shouldHave(text(expectedEmail));
+        $("p[id=currentAddress]").shouldHave(text(expectedCurrentAddress));
+        $("p[id=permanentAddress]").shouldHave(text(expectedPermanentAddress));
     }
 
     @Test // Отправка формы при некорректном значении Email
     void incorrectEmail(){
         open("/text-box.html");
-        $("input[id=userEmail]").setValue("F!");
+        $("input[id=userEmail]").setValue(incorrectUserEmail);
         $("button[id=submit]").click();
         $("p[id=email]").shouldNotHave(visible);
     }
