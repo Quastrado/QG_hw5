de2feed9c2b7830d1019c5eb8679c7a0b2366bb2
Quastrado <Ataxyswriter@yandex.ru>
2026-04-12 13:52:34 +0300

    remove comments

--- src/test/java/tests/testdata/TestDataPracticeForm.java
+++ src/test/java/tests/testdata/TestDataPracticeForm.java
@@ -6,12 +6,8 @@ public class TestDataPracticeForm {
             secondName = "Second Name",
             userEmail = "example@mail.com",
             userNumber = "9999999999",
-    //        dayOfBirth = '4',
-    //        subjects = 'Maths',
             uploadPicture = "src/test/java/tests/testdata/capybara.avif",
             currentAddress = "Pushkina, Kolotushkina",
-    //        state = 'Uttar Pradesh',
-    //        city = 'Agra',
             successSubmitText = "Thanks for submitting the form",
             formError = "Please fill required fields and enter a valid 10-digit mobile number.",
             incorrextUserNumber = "999999999";
