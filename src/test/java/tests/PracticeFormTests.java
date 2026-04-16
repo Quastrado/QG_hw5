733756fb2ad70978cedf7ce20dc6ed7f0aca1ffa
Quastrado <Ataxyswriter@yandex.ru>
2026-04-16 20:32:21 +0300

    close banner using jsCode

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -15,7 +15,8 @@ public class PracticeFormTests extends TestBase {
         // Открыть страницу с формой
         open("/automation-practice-form.html");
 //        // Убрать баннер
-        $("[aria-label=Close]").click();
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         // Заполнить поле FirstName
         $("[id=firstName]").click();
         $("[id=firstName]").setValue(firstName);
